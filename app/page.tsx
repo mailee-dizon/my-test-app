8acca6b90c266e67ee899d221927cd3e11992f9b
mailee-dizon <mailee.dizon05@gmail.com>
2025-04-25 14:28:39 -0700

    debug

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import dotenv from "dotenv";
 
 export default function Home() {
-  
+
+  console.log("Hello Mailee");
+  /*
   dotenv.config();
   console.log(process.env.TEST);
+  */
   return (
     <div>
       <h1>Testing {process.env.TEST}</h1>
